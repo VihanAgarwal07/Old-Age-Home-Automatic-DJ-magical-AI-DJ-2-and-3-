82004088231b25892f8ec0f385e0a3a00b9b1bbd
VihanAgarwal07 <78912707+VihanAgarwal07@users.noreply.github.com>
2021-11-19 18:06:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+leftWristScore=0;
+rightWristScore=0;
+
+var music1="";
+var music2="";
+function preload(){
+    //music1=loadSound("");
+    //music2=loadSound("")
+}
+function setup(){
+    canvas=createCanvas(700,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+     video.hide();
+
+     poseNet=ml5.poseNet(video,modelLoaded);
+     poseNet.on("pose",gotPoses);
+}
+function modelLoaded(){
+    console.log("Model is Ready");
+}
+function gotPoses(event){
+console.log(event);
+if(event.length>0){
+    //leftWristX=
+}
+}
+
+function draw(){
+    image(video,0,0,700,500);
+}
